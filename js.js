67764596ea9467ca1b233342f2f48e62b062fff5
SanaaWeee2006 <sanaquddus048@gmail.com>
2023-07-14 15:40:55 +0530

    first commit

---
+++ js.js
@@ -0,0 +1,19 @@
+var num=4
+let num-4
+let username="mahi"
+let 123user="mahi"
+let _123user="mahi"
+
+let num=4' user="mahi"
+
+area of circle 
+let radius=5,
+const pi=3.14
+area 
+radius=6
+pi=3.14
+area=pi*r*r
+const hobbies=['walking', 'reading', 'dancing']
+
+"5"+ mahi
+"5" 
